ef4722dd1d633afd9fccb8afaa858ce10e1e159e
itsmattsoria <matt.m.soria@gmail.com>
2021-05-10 18:27:30 -0500

    Updating deploy file

--- fabfile.py
+++ fabfile.py
@@ -13,6 +13,8 @@ def build():
 
 def deploy():
   update()
+  run('mkdir -p ' + env.remotepath + '/dist/assets/images/timeline')
+  put('dist/assets/images/timeline', env.remotepath + '/dist/assets/images/timeline/')
 
 def update():
   with cd(env.remotepath):
